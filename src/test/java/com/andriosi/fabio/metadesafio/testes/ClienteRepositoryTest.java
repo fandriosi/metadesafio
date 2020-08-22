2f48debed4721426c91c51069b627ca475fd6a19
fandriosi <fabio.andriosi@gmail.com>
2020-08-22 17:26:45 -0300

    commit 2020822 1630

--- src/test/java/com/andriosi/fabio/metadesafio/testes/ClienteRepositoryTest.java
+++ src/test/java/com/andriosi/fabio/metadesafio/testes/ClienteRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.andriosi.fabio.metadesafio.testes;
 
 import static org.junit.jupiter.api.Assertions.*;
-class ClienteControllerTest {
+class ClienteRepositoryTest {
   
 }
---
+++ src/test/java/com/andriosi/fabio/metadesafio/testes/ClienteTest.java
@@ -0,0 +1,55 @@
+package com.andriosi.fabio.metadesafio.testes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ClienteTest {
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getCpf() {
+    }
+
+    @Test
+    void setCpf() {
+    }
+
+    @Test
+    void getDataNacimento() {
+    }
+
+    @Test
+    void setDataNacimento() {
+    }
+
+    @Test
+    void getNaturalidade() {
+    }
+
+    @Test
+    void setNaturalidade() {
+    }
+
+    @Test
+    void getNacionalidade() {
+    }
+
+    @Test
+    void setNacionalidade() {
+    }
+}
